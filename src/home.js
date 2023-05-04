92408356365dc8e231819d41ed5de9f430cb72b7
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-05-04 15:34:08 +0200

    Rename element variables

--- src/home.js
+++ src/home.js
@@ -1,25 +1,25 @@
 export function createHome() {
   const mainDiv = document.querySelector('#content');
-  const heroDiv = document.createElement('div');
+  const homeDiv = document.createElement('div');
   const mainImgDiv = document.createElement('div');
   const mainTextDiv = document.createElement('div');
-  const heroLogo = document.createElement('div');
-  const heroText = document.createElement('div');
-  const heroImg = document.createElement('img');
-  const bigLogo = document.createElement('img');
-  heroImg.classList.add('hero-img');
-  heroDiv.classList.add('hero-div');
+  const heroLogoDiv = document.createElement('div');
+  const heroBlurb = document.createElement('div');
+  const burgerImg = document.createElement('img');
+  const bigLogoImg = document.createElement('img');
+  burgerImg.classList.add('burger-img');
+  homeDiv.classList.add('home-div');
   mainImgDiv.classList.add('main-img-div');
-  mainImgDiv.appendChild(heroImg);
+  mainImgDiv.appendChild(burgerImg);
   mainTextDiv.classList.add('main-text-div');
-  heroLogo.classList.add('hero-logo');
-  bigLogo.classList.add('big-logo');
-  heroLogo.appendChild(bigLogo);
-  heroText.classList.add('hero-text');
-  heroText.textContent = "Get ready to burger out at burgerburgerburgers! Our patties are a cut above the rest, and we've got buns of steel to match. Come see why our burgers are flippin' amazing!"
-  mainDiv.appendChild(heroDiv);
-  heroDiv.appendChild(mainImgDiv);
-  heroDiv.appendChild(mainTextDiv);
-  mainTextDiv.appendChild(heroLogo);
-  mainTextDiv.appendChild(heroText);
+  heroLogoDiv.classList.add('hero-logo-div');
+  bigLogoImg.classList.add('big-logo-img');
+  heroLogoDiv.appendChild(bigLogoImg);
+  heroBlurb.classList.add('hero-blurb');
+  heroBlurb.textContent = "Get ready to burger out at burgerburgerburgers! Our patties are a cut above the rest, and we've got buns of steel to match. Come see why our burgers are flippin' amazing!"
+  mainDiv.appendChild(homeDiv);
+  homeDiv.appendChild(mainImgDiv);
+  homeDiv.appendChild(mainTextDiv);
+  mainTextDiv.appendChild(heroLogoDiv);
+  mainTextDiv.appendChild(heroBlurb);
 };
