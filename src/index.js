0da5efc4f8304979e5bd83a3522bd1614242a241
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-05-04 15:59:08 +0200

    Create button functions to switch between modules

--- src/index.js
+++ src/index.js
@@ -4,5 +4,33 @@ import { createMenu } from './menu.js';
 import { createContact } from './contact.js';
 import './style.css';
 
+function openHome() {
+  mainDiv.removeChild(mainDiv.lastChild);
+  homeBtn.classList.add('btn-current');
+  menuBtn.classList.remove('btn-current');
+  contactBtn.classList.remove('btn-current');
+  createHome();
+}
+
+function openMenu() {
+  mainDiv.removeChild(mainDiv.lastChild);
+  homeBtn.classList.remove('btn-current');
+  menuBtn.classList.add('btn-current');
+  contactBtn.classList.remove('btn-current');
+  createMenu();
+}
+
+function openContact() {
+  mainDiv.removeChild(mainDiv.lastChild);
+  homeBtn.classList.remove('btn-current');
+  menuBtn.classList.remove('btn-current');
+  contactBtn.classList.add('btn-current');
+  createContact();
+}
+
+homeBtn.addEventListener('click', openHome);
+menuBtn.addEventListener('click', openMenu);
+contactBtn.addEventListener('click', openContact);
+
 createHeader();
 createHome();
